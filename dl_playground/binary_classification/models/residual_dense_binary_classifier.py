3267153814879a19e3e9ee691fa2f9636a9c8fa5
RoiNeuman <roi.neuman@gmail.com>
2021-02-28 13:11:20 +0200

    Residual_dense model can now handle upscaling and downscaling of dimensions.

--- dl_playground/binary_classification/models/residual_dense_binary_classifier.py
+++ dl_playground/binary_classification/models/residual_dense_binary_classifier.py
@@ -1,7 +1,8 @@
 from typing import List
 
 from tensorflow.python.keras import Input
-from tensorflow.python.keras.layers import Dense, BatchNormalization, Dropout, Add, Activation
+from tensorflow.python.keras.layers import Dense, BatchNormalization, Dropout, Add, Activation, ZeroPadding1D, \
+    MaxPooling1D, Reshape, Flatten
 from tensorflow.python.keras.models import Model
 from tensorflow.python.keras.regularizers import l2
 
@@ -16,6 +17,8 @@ def residual_dense_binary_classifier(input_dim: int,
 
     for layer_units in hidden_layers_units:
         X_shortcut = X
+
+        # The main path
         X = Dense(layer_units,
                   kernel_initializer='normal',
                   kernel_regularizer=l2(l2_regularizer_param),
@@ -25,8 +28,27 @@ def residual_dense_binary_classifier(input_dim: int,
             X = Dropout(dropout)(X)
         if batch_norm:
             X = BatchNormalization(axis=1)(X)
-        if X.shape[1] == X_shortcut.shape[1]:
-            X = Add()([X, X_shortcut])
+
+        # The shortcut path
+        if X_shortcut.shape[1] < layer_units:
+            # Adding dimensions
+            X_shortcut = Reshape((X_shortcut.shape[1], 1), input_shape=X_shortcut.shape)(X_shortcut)
+            X_shortcut = ZeroPadding1D(padding=(0, layer_units - X_shortcut.shape[1]))(X_shortcut)
+            X_shortcut = Flatten()(X_shortcut)
+        elif X_shortcut.shape[1] > layer_units:
+            # Reducing dimensions
+            pool_size = X_shortcut.shape[1] - layer_units + 1
+            X_shortcut = Reshape((X_shortcut.shape[1], 1), input_shape=X_shortcut.shape)(X_shortcut)
+            X_shortcut = MaxPooling1D(pool_size=pool_size,
+                                      strides=1,
+                                      padding='valid',
+                                      data_format='channels_last')(X_shortcut)
+            X_shortcut = Flatten()(X_shortcut)
+
+        # Combining the main and shortcut paths
+        X = Add()([X, X_shortcut])
+
+        # Adding non-linear activation
         X = Activation('relu')(X)
 
     # Output layer
